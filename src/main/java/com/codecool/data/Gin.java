f0df7f5d340a722171f44160ba9f26be3c288daf
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2024-10-24 10:44:03 +0200

    Game logic extended

---
+++ src/main/java/com/codecool/data/Gin.java
@@ -0,0 +1,7 @@
+package com.codecool.data;
+
+public class Whisky extends Drink {
+    public Whisky(String name, int price, int alcPercentage, int amountLeft) {
+        super(name, price, alcPercentage, amountLeft);
+    }
+}
